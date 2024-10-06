8b58704de0e238e68de1dfc4a7c2abeae33475aa
lostwaltz <lostwaltz89@gmail.com>
2024-10-06 22:05:11 +0900

    init Project_2D_TopDownShooting

---
+++ 2D_Project/2DTopDownShooting/Assets/Scripts/TopDownAimRotation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer armRenderer;
+    [SerializeField] private Transform armPivot;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direcation)
+    {
+        RotateArm(direcation);
+    }
+
+    private void RotateArm(Vector2 direcation)
+    {
+        float rotZ = Mathf.Atan2(direcation.y, direcation.x) * Mathf.Rad2Deg;
+
+        Debug.Log(rotZ);
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+
+        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
---
+++ 2D_Project/2DTopDownShooting/Assets/Scripts/TopDownShooting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TopDownShooting : MonoBehaviour
+{
+    private TopDownController controller;
+
+    [SerializeField] private Transform projectileSpawnPostion;
+    private Vector2 aimDirection = Vector2.right;
+
+    public GameObject TestPrefab;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnAttackEvent += OnShoot;
+
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        aimDirection = direction;
+    }
+
+    private void OnShoot()
+    {
+        CreateProjectile();
+    }
+
+    private void CreateProjectile()
+    {
+        // TODO :: 투사체 날라가게
+        Instantiate(TestPrefab, projectileSpawnPostion.position, Quaternion.identity);
+    }
+}
